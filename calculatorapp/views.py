9d605ee4d3827a4fbce099dada10ce0799143f32
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2024-02-05 12:55:06 +0545

    create a simple calculator with django

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -2,4 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    pass
+    return render(request,'home.html')
+
+def result(request):
+    num1 = int(request.GET.get('num1'))
+    num2 = int(request.GET.get('num2'))
+
+    if request.GET.get('add') == "":
+        ans = num1 + num2
+        operation = '+'
+
+    elif request.GET.get('subtract') == "":
+        ans = num1 - num2
+        operation = '-'
+
+    elif request.GET.get('multiply') == "":
+        ans = num1 * num2
+        operation = '*'
+
+    else:
+        ans = num1/num2
+        operation = '/'
+    
+    context={
+        'first': num1,
+        'second': num2,
+        'condition': operation,
+        'answer': ans,
+    }
+
+    return render(request, 'result.html', context)
